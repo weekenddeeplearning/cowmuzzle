d8e2955c10a92586039c8f8b6948f3b9f7b223ec
weekenddeeplearning <66552197+weekenddeeplearning@users.noreply.github.com>
2020-07-02 12:41:02 +0530

    Add files via upload

--- static/asserts/custom.js
+++ static/asserts/custom.js
@@ -20,7 +20,7 @@ $( document ).ready(function() {
 
               $.ajax({
                 type: "POST",
-                url: 'http://127.0.0.1:5000/find',
+                url: '/find',
                 data: formData,
                 async: false,
                 contentType: false,
